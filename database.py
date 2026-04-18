643ec0cb3d35ba00397a49f2a9ef648e0c8f1015
SatyamSoni <satyamsoni853@gmail.com>
2026-04-18 16:47:39 +0530

    Fixed missing DATABASE_URL error by adding SQLite fallback

--- database.py
+++ database.py
@@ -6,10 +6,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
-# For PostgreSQL, we don't need check_same_thread
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Handle SQLite specific arguments
+connect_args = {"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
